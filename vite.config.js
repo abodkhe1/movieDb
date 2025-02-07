94c6cc60782213928b410e5be7b27ebd7315a15f
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2025-02-07 10:17:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: ["wpxs6r-5173.csb.app"] 
+  }
 })
