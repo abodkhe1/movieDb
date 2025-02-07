5b1289979934729c25221c293f1296b84884ebc1
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2025-02-07 10:44:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-       host: true, 
-    allowedHosts: "all"
+    host: true,
+    allowedHosts: "all",
+    cors: true,
+    strictPort: true
   }
 })
+
 
